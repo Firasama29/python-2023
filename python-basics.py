57c886e21dad92a7046fe9c93bc7ed39145686eb
Firasama29 <ferasama@gmail.com>
2023-08-22 21:27:42 +0800

    first push of python basics

--- python-basics.py
+++ python-basics.py
@@ -1,1 +1,36 @@
-#
+#integer (whole number)
+myInt = 10
+print('integer: ', myInt)
+
+#floating point (decimal)
+myFloat = 20.5
+print('float:', myFloat)
+
+#string with single quotes
+print('string with single quotes')
+
+#string with double quotes
+print("string with double quotes")
+
+#using operators with numbers
+print(myInt + myFloat)
+
+#using operators with strings
+print('Hello' + ' ' + 'World!')
+
+#lists
+myList = []
+
+#add items
+myList.append(1)
+myList.append(2)
+myList.append(3)
+myList.append(4)
+print("list: ", myList)
+print("first item in list: ", myList[0])
+
+#for loop
+for i in myList:
+    print(i)
+
+    
